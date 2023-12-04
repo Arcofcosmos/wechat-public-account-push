91519ec9b7baefa591b9594cb19ad58339cf3113
良风抚旧 <67370181+Arcofcosmos@users.noreply.github.com>
2023-12-04 23:09:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'og-xy6ZR8ocyEUQp_xUAWVjzLX4c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'taXmo49YQ1usEDBbc8_daQcYVdjDsOO8dOqegqxuawo',
+      useTemplateId: 'TZL-1JbchX4L4QNa7iWDLPdlo0zEgxe1q3QLZ57vsw4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-16',
       festivals: [
