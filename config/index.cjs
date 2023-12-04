d90330a944edb4c8de0e70eb873e90400d0d917f
良风抚旧 <67370181+Arcofcosmos@users.noreply.github.com>
2023-12-04 22:58:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'og-xy6ZR8ocyEUQp_xUAWVjzLX4c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'LhlcEzW75_vn72drS-sDXCkpVipvsCnNwbBzOyIQuO8',
+      useTemplateId: 'taXmo49YQ1usEDBbc8_daQcYVdjDsOO8dOqegqxuawo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-16',
       festivals: [
@@ -38,13 +38,6 @@ const USER_CONFIG = {
         {
           type: '节日', name: '元旦', year: '2024', date: '01-01',
         },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
